c7eab3d4175819c789d5444a11af11ac98586b61
Thor77 <thor77@thor77.org>
2019-08-01 20:31:35 +0200

    Add puppet strings to missing parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,9 @@
 # @param version [String] Version to install
 # @param repo [String] GitHub repo to download release from
 # @param install_path [String] Directory to install binary into
+# @param config_path [String] Path to g10k config file
+# @param cfg [Hash] Config options (beside sources)
+# @param sources [Hash] Configuration for sources
 class g10k (
   String $version = $g10k::params::version,
   String $repo = $g10k::params::repo,
